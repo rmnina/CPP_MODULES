6eb0775889e48277e7b9486c353ebf9eba669330
jdufour <jdufour@made-f0Dr7s3.clusters.42paris.fr>
2024-04-25 18:10:41 +0200

    first commit

---
+++ CPP_00/ex01/include/Contact.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 17:00:31 by jdufour           #+#    #+#             */
+/*   Updated: 2024/04/25 18:02:59 by jdufour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+
+class Contact
+{
+
+	public:
+
+	Contact(void);
+	~Contact(void);
+};
+
+#endif
