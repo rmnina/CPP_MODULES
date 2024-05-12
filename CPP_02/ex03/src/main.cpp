4fe027a1ecb2eda73462d7855d937e2a9f8ca17a
rmnina <jovicadufour@gmail.com>
2024-05-12 21:45:51 +0200

    fix main

--- CPP_02/ex03/src/main.cpp
+++ CPP_02/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:34:36 by jdufour           #+#    #+#             */
-/*   Updated: 2024/05/12 21:44:16 by jdufour          ###   ########.fr       */
+/*   Updated: 2024/05/12 21:45:31 by jdufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main( void )
 	Point	c(1.7, -4.65);
 	Point	point(4.12, -4.39);
 
-	if (bsp(a, b, d, point) == true)
+	if (bsp(a, b, c, point))
 		std::cout << "The point is " << BOLD << GREEN << "inside " << RESET << "the triangle." << std::endl;
 	else
 		std::cout << "The point is " << BOLD << RED << "outside " << RESET << "(or on) the triangle." << std::endl;
