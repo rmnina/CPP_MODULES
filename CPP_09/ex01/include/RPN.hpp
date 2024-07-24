31528b685fa2f7a7c24bc36f4cee06f6f36b84bc
Jovica <jovicadufour@gmail.com>
2024-07-25 01:06:03 +0200

    fancy arg error

--- CPP_09/ex01/include/RPN.hpp
+++ CPP_09/ex01/include/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 21:45:33 by jdufour           #+#    #+#             */
-/*   Updated: 2024/07/25 00:52:01 by jdufour          ###   ########.fr       */
+/*   Updated: 2024/07/25 00:59:59 by jdufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,19 @@
 # include <string>
 # include <iostream>
 # include <exception>
+
+# define RED		"\x1b[31m"
+# define BLUE		"\x1b[34m"
+# define RESET		"\x1b[0m"
+# define GREEN		"\x1b[32m"
+# define PINK		"\x1b[95m"
+# define YELLOW 	"\x1b[33m"
+# define ORANGE 	"\x1b[38;5;208m"
+# define PURPLE 	"\x1b[38;5;129m"
+# define TURQUOISE	"\x1b[38;5;80m"
+# define MAGENTA	"\x1b[35m"
+# define BOLD		"\x1b[1m"
+# define ITALIC		"\x1b[3m"
 
 class RPN
 {
--- CPP_09/ex01/src/main.cpp
+++ CPP_09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 22:57:05 by jdufour           #+#    #+#             */
-/*   Updated: 2024/07/25 00:53:04 by jdufour          ###   ########.fr       */
+/*   Updated: 2024/07/25 01:03:58 by jdufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
+	{
+		std::cerr << BOLD RED << "Error : RPN needs one argument." << std::endl;
+		std::cerr << "Format : \"[digits] [operands] [digits] [operands] [...]\"." << std::endl << std::endl;
+		std::cerr << RESET ITALIC BLUE << "You must provide " << BOLD << "n digits " << RESET ITALIC BLUE << "and " << BOLD << "n - 1 operands " << RESET ITALIC BLUE << "for the program to run." << std::endl;
+		std::cerr << "Any other character than " << BOLD << "\"123456789+-/* \" " << RESET ITALIC BLUE << "is not accepted." << std::endl;
+		std::cerr << "If you enter 2 or more digits together, " << BOLD << "they will be treated separatly. " << RESET ITALIC BLUE << "Ex : \"12 +\" will be treated as \"1 2 +\"." << RESET << std::endl;
 		return (1);
-
+	}
 	RPN	test;
 	try
 	{
